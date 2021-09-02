7d87a103237e61ec419024e310f225532ca4a1b3
Alexander <shalek.edu@mail.ru>
2021-09-02 19:36:03 +1000

    implement copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"os"
+	"io"
 )
 
 var (
@@ -10,6 +12,49 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	srcStat, err := src.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !srcStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > srcStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = src.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		if _, err = io.Copy(dst, src); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+	} else {
+		if _, err = io.CopyN(dst, src, limit); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -4,9 +4,13 @@ import (
 	"testing"
 	"io/ioutil"
 	"bytes"
+	"os"
+	"path"
 )
 
 func TestCopy(t *testing.T) {
+	fromPath := "./testdata/input.txt"
+	tmpDir := os.TempDir()
 	type args struct {
 		fromPath string
 		toPath   string
@@ -22,8 +26,8 @@ func TestCopy(t *testing.T) {
 		{
 			name: "offset0_limit0",
 			args: args{
-				fromPath: "./testdata/input.txt",
-				toPath:   "./testdata/out_offset0_limit0_result.txt",
+				fromPath: fromPath,
+				toPath:   "out_offset0_limit0_result.txt",
 				offset:   0,
 				limit:    0,
 			},
@@ -31,23 +35,132 @@ func TestCopy(t *testing.T) {
 			wantPath:   "./testdata/out_offset0_limit0.txt",
 
 		},
+		{
+			name: "offset0_limit10",
+			args: args{
+				fromPath: fromPath,
+				toPath:   "out_offset0_limit10_result.txt",
+				offset:   0,
+				limit:    10,
+			},
+			wantErr: false,
+			wantPath:   "./testdata/out_offset0_limit10.txt",
+
+		},
+		{
+			name: "offset0_limit1000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   "out_offset0_limit1000_result.txt",
+				offset:   0,
+				limit:    1_000,
+			},
+			wantErr: false,
+			wantPath:   "./testdata/out_offset0_limit1000.txt",
+
+		},
+		{
+			name: "offset0_limit10000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   "out_offset0_limit10000_result.txt",
+				offset:   0,
+				limit:    10_000,
+			},
+			wantErr: false,
+			wantPath:   "./testdata/out_offset0_limit10000.txt",
+
+		},
+		{
+			name: "offset100_limit1000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   "out_offset100_limit1000_result.txt",
+				offset:   100,
+				limit:    1_000,
+			},
+			wantErr: false,
+			wantPath:   "./testdata/out_offset100_limit1000.txt",
+
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+			toPath := path.Join(tmpDir, tt.args.toPath)
+			if err := Copy(tt.args.fromPath, toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
 				t.Fatalf("Copy() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
 			origin, err := ioutil.ReadFile(tt.wantPath)
 			if err != nil {
-				t.Fatalf(".ReadAll(tt.wantPath) error = %v, want nil", err)
+				t.Fatalf(".ReadFile(tt.wantPath) error = %v, want nil", err)
 			}
-			result, err := ioutil.ReadFile(tt.args.toPath)
+			result, err := ioutil.ReadFile(toPath)
 			if err != nil {
-				t.Fatalf(".ReadAll(tt.args.toPath) error = %v, want nil", err)
+				t.Fatalf(".ReadFile(tt.args.toPath) error = %v, want nil", err)
 			}
 			if !bytes.Equal(origin, result) {
 				t.Fatalf("files are not equal")
+			}
+		})
+	}
+}
+
+func TestCopy_ErrorCases(t *testing.T) {
+	fromPath := "./testdata/input.txt"
+	toPathFilename := "error.txt"
+	tmpDir := os.TempDir()
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    error
+	}{
+		{
+			name: "offset exceeds file size",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPathFilename,
+				offset:   1_000_000,
+				limit:    10,
+			},
+			wantErr: ErrOffsetExceedsFileSize,
+
+		},
+		{
+			name: "unsupported file: directory",
+			args: args{
+				fromPath: "./testdata",
+				toPath:   toPathFilename,
+				offset:   0,
+				limit:    10,
+			},
+			wantErr: ErrUnsupportedFile,
+
+		},
+		{
+			name: "unsupported file: character device",
+			args: args{
+				fromPath: "./testdata/device_c",
+				toPath:   toPathFilename,
+				offset:   0,
+				limit:    10,
+			},
+			wantErr: ErrUnsupportedFile,
+
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			toPath := path.Join(tmpDir, tt.args.toPath)
+			gotErr := Copy(tt.args.fromPath, toPath, tt.args.offset, tt.args.limit)
+			if nil == gotErr || gotErr != tt.wantErr {
+				t.Fatalf("Copy() error = %v, wantErr %v", gotErr, tt.wantErr)
 			}
 		})
 	}
