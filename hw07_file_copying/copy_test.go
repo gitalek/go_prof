576b3452c056111d24f19ebddc1dd3435c515981
Alexander <shalek.edu@mail.ru>
2021-09-02 17:56:45 +1000

    add test case "offset0_limit0"

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"io/ioutil"
+	"bytes"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    bool
+		wantPath string
+	}{
+		{
+			name: "offset0_limit0",
+			args: args{
+				fromPath: "./testdata/input.txt",
+				toPath:   "./testdata/out_offset0_limit0_result.txt",
+				offset:   0,
+				limit:    0,
+			},
+			wantErr: false,
+			wantPath:   "./testdata/out_offset0_limit0.txt",
+
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+				t.Fatalf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			origin, err := ioutil.ReadFile(tt.wantPath)
+			if err != nil {
+				t.Fatalf(".ReadAll(tt.wantPath) error = %v, want nil", err)
+			}
+			result, err := ioutil.ReadFile(tt.args.toPath)
+			if err != nil {
+				t.Fatalf(".ReadAll(tt.args.toPath) error = %v, want nil", err)
+			}
+			if !bytes.Equal(origin, result) {
+				t.Fatalf("files are not equal")
+			}
+		})
+	}
 }
