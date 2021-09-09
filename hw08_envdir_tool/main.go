5db9161ba33cee6dc2853251aa273f3b848bad31
Alexander <shalek.edu@mail.ru>
2021-09-10 02:10:39 +1000

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	l := len(os.Args)
+	if l < 2 {
+		log.Fatalln(
+			"the path to env dir and command are not specified: go-envdir /path/to/env/dir command",
+		)
+	}
+	if l < 3 {
+		log.Fatalln("the command is not specified: go-envdir /path/to/env/dir command")
+	}
+
+	envdir, command := os.Args[1], os.Args[2:]
+
+	env, err := ReadDir(envdir)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	retCode := RunCmd(command, env)
+	os.Exit(retCode)
 }
